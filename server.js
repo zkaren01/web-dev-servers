04ba1b7b29a054669aebebf0aef9ba096793001c
Karen Zhu <68167276+zkaren01@users.noreply.github.com>
2022-04-13 21:26:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import mongoose from "mongoose";
-
+const app = express();
 const CONNECTION_STRING = 'mongodb+srv://zkaren01:6fvRk6J4HreZzdhB@cluster0.vadh9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-|| 'mongodb://localhost:27017/webdev'
+    || 'mongodb://localhost:27017/webdev'
 mongoose.connect(CONNECTION_STRING);
 
-const app = express();
 app.use(cors());
 app.use(express.json());
-helloController(app);
-userController(app);
-tuitsController(app);
-// app.get('/hello', (req, res) => {res.send('Hello World!')})
-// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+
+require(helloController)(app);
+require(userController)(app);
+require(tuitsController)(app);
+app.get('/hello', (req, res) => {res.send('Hello World!')})
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 app.listen(process.env.PORT || 4000);
